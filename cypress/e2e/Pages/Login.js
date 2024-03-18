95468efc6d7e33736ffbd1d7f736188b896d1c41
Harman-cr7 <harmanghuman95@gmail.com>
2024-03-17 21:06:02 -0400

    Called the class function

--- cypress/e2e/Pages/Login.js
+++ cypress/e2e/Pages/Login.js
@@ -18,13 +18,13 @@ export class login{
 
     // }
 
-    enterUsername(){
-        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    enterUsername(username){
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(username)
  
     }
 
-    enterPassword(){
-        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    enterPassword(password){
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password)
        
     }
 
--- cypress/e2e/pomdemo.cy.js
+++ cypress/e2e/pomdemo.cy.js
@@ -7,8 +7,8 @@ const Login = new login()
 it('POM Demo', function(){
 
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    Login.enterUsername()
-    Login.enterPassword()
+    Login.enterUsername('Admin')
+    Login.enterPassword('admin123')
     Login.clickLogin()
 
 
